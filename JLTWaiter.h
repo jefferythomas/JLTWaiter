878f49ab698ed100e82b080beaa593659cbc3e14
Jeffery Thomas <jeffery.thomas@gmail.com>
2013-03-26 01:31:38 -0400

    Added the initial version of JLTWaiter.

---
+++ JLTWaiter.h
@@ -0,0 +1,33 @@
+//
+//  JLTWaiter.h
+//  JLTWaiter
+//
+//  Created by Jeffery Thomas on 3/25/13.
+//  Copyright (c) 2013 JLTSource. No rights reserved. Do with it what you will.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void(^JLTWaiterBlock)();
+
+@interface JLTWaiter : NSObject
+
+@property (assign, atomic, getter=isFinished) BOOL finished;
+@property (assign, atomic) NSTimeInterval waitQuantum;
+
+- (void)waitWithTimeOut:(NSTimeInterval)timeOut;
+- (void)waitWithTimeOut:(NSTimeInterval)timeOut atQuantumPeformBlock:(JLTWaiterBlock)block;
+
++ (JLTWaiter *)waiter;
++ (JLTWaiter *)waiterWithWaitQuantum:(NSTimeInterval)waitQuantum;
+- (id)initWithWaitQuantum:(NSTimeInterval)waitQuantum;
+
+@end
---
+++ JLTWaiter.m
@@ -0,0 +1,66 @@
+//
+//  JLTWaiter.m
+//  JLTWaiter
+//
+//  Created by Jeffery Thomas on 3/25/13.
+//  Copyright (c) 2013 JLTSource. No rights reserved. Do with it what you will.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+#import "JLTWaiter.h"
+
+@implementation JLTWaiter
+
+- (void)waitWithTimeOut:(NSTimeInterval)timeOut
+{
+    [self waitWithTimeOut:timeOut atQuantumPeformBlock:NULL];
+}
+
+- (void)waitWithTimeOut:(NSTimeInterval)timeOut atQuantumPeformBlock:(JLTWaiterBlock)block
+{
+    NSDate *timeOutDate = [NSDate dateWithTimeIntervalSinceNow:timeOut];
+
+    if (block) block();
+
+    while (!self.finished && [timeOutDate timeIntervalSinceNow] > 0) {
+        NSDate *date = [NSDate dateWithTimeIntervalSinceNow:self.waitQuantum];
+        [[NSRunLoop currentRunLoop] runUntilDate:date];
+
+        if (block) block();
+    }
+}
+
++ (JLTWaiter *)waiter
+{
+    return [[JLTWaiter alloc] init];
+}
+
++ (JLTWaiter *)waiterWithWaitQuantum:(NSTimeInterval)waitQuantum
+{
+    return [[JLTWaiter alloc] initWithWaitQuantum:waitQuantum];
+}
+
+- (id)initWithWaitQuantum:(NSTimeInterval)waitQuantum
+{
+    self = [super init];
+    if (self) {
+        _waitQuantum =  waitQuantum;
+    }
+    return self;
+}
+
+#pragma mark Memory Lifecycle
+
+- (id)init
+{
+    return [self initWithWaitQuantum:0.1];
+}
+
+@end
